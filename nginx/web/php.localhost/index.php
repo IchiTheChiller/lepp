acef13b60cf9de11c3ee63e8f7750e4b06c36be7
ichithechiller <ichithechiller@tutamail.com>
2025-04-28 10:37:13 +0200

    $Var and nl2br()

--- nginx/web/php.localhost/index.php
+++ nginx/web/php.localhost/index.php
@@ -1,2 +1,22 @@
-<?php $title = "Everwinter"; ?>
-<h1><?php echo $title; ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php
+    // Single Line Comments
+    /* 
+    Multi Line Comments
+     */
+    $Var = "Hi";
+     echo nl2br("Hello, PHP!\n");
+     echo nl2br("Hello, PHP!\n");
+     echo $Var 
+     ?>
+     </h1>
+</body>
+</html>
